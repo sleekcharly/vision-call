133a5116361ce6336c81e33aa1e27d5e7fee9e90
sleekcharly <c_ukasoanya@yahoo.com>
2024-04-03 12:27:20 +0100

    created authentication and middleware using clerk

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import MobileNav from './MobileNav';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 
 const Navbar = () => {
   return (
@@ -21,6 +22,9 @@ const Navbar = () => {
 
       <div className="flex-between gap-5">
         {/* Clerk user management */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <MobileNav />
       </div>
     </nav>
