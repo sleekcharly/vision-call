dcf86c03f55ba3f53710654d08eb6e0eaf49e607
sleekcharly <c_ukasoanya@yahoo.com>
2024-04-09 09:20:12 +0100

    created the meeting room component and its functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider';
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react';
+
+export const metadata: Metadata = {
+  title: 'VISION CALL',
+  description: 'A video calling app',
+  icons: {
+    icon: '/icons/logo.svg',
+  },
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
